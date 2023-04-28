a042e8a2853a763b3958d56db0b2b7075ad2cd65
Julia <julianna1708qa@gmail.com>
2023-04-28 19:24:35 -0400

    added new Logout API test

---
+++ src/test/java/api_tests/LogoutAPITest.java
@@ -0,0 +1,22 @@
+package api_tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LogoutAPITest {
+    @Test
+    public void Logout_API_TestingThatIsPassing(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Logout_API_TestingThatIsFailing(){
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void Logout_API_TestingThatIsPassing2(){
+        Assert.assertTrue(true);
+    }
+
+}
